dc200de7351a88202329ffba3ff37a748f165c74
EC2 Default User <ec2-user@ip-172-31-30-186.us-west-2.compute.internal>
2019-04-19 13:51:31 +0000

    image change

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker push sumantatwm/multi-server:$SHA
 docker push sumantatwm/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=sumantatwm/multi-server:$SHA
-kubectl set image deployments/client-deployment client=sumantatwm/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=sumantatwm/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
